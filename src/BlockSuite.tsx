0427667612f5d3e146c28ca7ba1a145b7cb482c5
tzhangchi <terry.zhangchi@outlook.com>
2022-10-28 23:01:43 +0800

    update lastest blocksuite init flow

--- src/BlockSuite.tsx
+++ src/BlockSuite.tsx
@@ -3,10 +3,39 @@ import "@blocksuite/editor";
 import "@blocksuite/blocks/style";
 import "./index.css";
 
-console.log("load BlockSuite");
+import type { EditorContainer } from "@blocksuite/editor";
+import { createEditor } from "@blocksuite/editor";
+import { forwardRef, Suspense, useEffect, useRef } from "react";
+
+// eslint-disable-next-line react/display-name
+const BlockSuiteEditor = forwardRef<EditorContainer>(({}, ref) => {
+  const containerElement = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!containerElement.current) {
+      return;
+    }
+    const editor = createEditor();
+    containerElement.current.appendChild(editor);
+    if (ref) {
+      if ("current" in ref) {
+        ref.current = editor;
+      } else {
+        ref(editor);
+      }
+    }
+    return () => {
+      editor.remove();
+    };
+  }, [ref]);
+  return <div id="editor" style={{ height: "100%" }} ref={containerElement} />;
+});
 function BlockSuite() {
   //@ts-ignore
-  return <editor-container></editor-container>;
+  return (
+    <Suspense>
+      <BlockSuiteEditor />
+    </Suspense>
+  );
 }
 
 export { BlockSuite };
